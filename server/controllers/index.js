67b35528e8684d280946b091a06df5bd920a3ecc
terenceDev <terence.arseneault@hotmail.com>
2023-12-14 19:14:23 -0400

    installed vite to client side also installed cors on server side

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
 const apiRoutes = require("./api");
+const homeRoutes = require("./home_routes");
 
 router.use("/api", apiRoutes);
+router.use("/", homeRoutes);
 
 module.exports = router;
